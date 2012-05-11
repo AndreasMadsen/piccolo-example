036a5a45ccc09f8567741a80ab57111b7bbc6986
Andreas Madsen <amwebdk@gmail.com>
2012-05-11 20:35:37 +0200

    [update] to use piccolo 0.2.0

--- project.js
+++ project.js
@@ -1,9 +1,39 @@
 
+var path = require('path');
+
 var piccolo = require('piccolo');
-var http = require('http');
 
 var project = piccolo();
+
     project.on('ready', function () {
       console.log('piccolo server is ready');
     });
-    project.attach(http.createServer().listen(8000));
+
+    project.configure(function () {
+      project.set('reload', 'auto');
+
+      project.set('root', path.dirname(module.filename));
+      project.set('temp', './temp');
+
+      project.set('static', './static');
+      project.set('template', './template');
+      project.set('modules', './modules');
+    });
+
+    project.configure('development', function () {
+      project.set('cache', 'none');
+    });
+
+    project.configure('production', function () {
+      project.set('cache', (60*60*24));
+
+      /** not implemented
+       * project.compile('static');
+       * project.compile('template');
+       * project.compile('modules');
+       */
+    });
+
+    project.use('development');
+
+    project.listen(8000);
